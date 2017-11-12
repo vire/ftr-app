9fe4ba00cedafe977107d29049482a70412e56f2
vire <code.vire@gmail.com>
2017-11-12 15:33:59 +0100

    chore(fmt): Update prettier, add husky, setup hooks, server

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,10 @@ import * as React from 'react'
 
 import Layout from '../components/Layout'
 
-const IndexPage = () => (<Layout><h1 className="u-centered">Welcome to FTR App</h1></Layout>)
+const IndexPage = () => (
+  <Layout>
+    <h1 className="u-centered">Welcome to FTR App</h1>
+  </Layout>
+)
 
 export default IndexPage
